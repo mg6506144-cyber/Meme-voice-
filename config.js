bf0ac9b0f6b844f8d1cde76a756df5ec67c562c9
mg6506144-cyber <mg6506144@gmail.com>
2026-05-17 21:28:56 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-export const BOT_TOKEN = "8836028231:AAGsYV-1p-RC0pqt7RfmCmGipsqaKGLZjzg";
+module.exports = {
+  BOT_TOKEN: "8836028231:AAGsYV-1p-RC0pqt7RfmCmGipsqaKGLZjzg",
+  WATERMARK_TEXT: "Mdraju_3m"
+};
